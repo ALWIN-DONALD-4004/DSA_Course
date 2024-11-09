a94f1df0261dc21bb03b1405d605891da84b7f24
alwindonald <alwindonald4004@gmail.com>
2024-11-09 21:24:27 +0530

    first commit

--- Dsa/src/com/dsa/patterns/PatternDemo.java
+++ Dsa/src/com/dsa/patterns/PatternDemo.java
@@ -11,18 +11,17 @@ public class PatternDemo {
 		System.out.print("Enter no of count : ");
 		int n = sc.nextInt();
 		
-//		new PatternOne(n);
-//		new PatternTwo(n);
-//		new PatternThree(n);
-//		new PatternFour(n);
-//		new PatternFive(n);
-//		new PatternSix(n);
-//		new PatternSeven(n);
-//		
-//		new PatternEight(n);
-//		new PatternNine(n);
-//		new PatternTen(n);
-//		new PatternEleven(n);
+		new PatternOne(n);
+		new PatternTwo(n);
+		new PatternThree(n);
+		new PatternFour(n);
+		new PatternFive(n);
+		new PatternSix(n);
+		new PatternSeven(n);
+		new PatternEight(n);
+		new PatternNine(n);
+		new PatternTen(n);
+		new PatternEleven(n);
 		new PatternTwelve(n);
 		
 		
